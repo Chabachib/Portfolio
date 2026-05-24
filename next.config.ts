5976e015bcaf42cb36bcac5ae767c4486c847e17
Chabachib <nouhtahachebchoub@gmail.com>
2026-05-24 22:02:56 +0100

    Portfoliooo

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,10 @@
 import type { NextConfig } from 'next'
 
-const nextConfig: NextConfig = {}
+const nextConfig: NextConfig = {
+  output: 'export',
+  basePath: '/Portfolio',
+  assetPrefix: '/Portfolio/',
+  images: { unoptimized: true },
+}
 
 export default nextConfig
